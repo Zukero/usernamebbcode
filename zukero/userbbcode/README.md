96fcf35bdfa062c2e527fd56b635335d1f6dc699
Zukero <kpochat@gmail.com>
2017-12-26 16:21:12 +0100

    Initial commit

---
+++ zukero/userbbcode/README.md
@@ -0,0 +1,11 @@
+# User BBCode
+
+## Installation
+
+Copy the extension to phpBB/ext/zukero/userbbcode
+
+Go to "ACP" > "Customise" > "Extensions" and enable the "User BBCode" extension.
+
+## License
+
+[GPLv2](license.txt)
---
+++ zukero/userbbcode/event/main_listener.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ *
+ * User BBCode. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2017, Zukero, github.com/Zukero
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace zukero\userbbcode\event;
+
+/**
+ * @ignore
+ */
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * User BBCode Event listener.
+ */
+class main_listener implements EventSubscriberInterface
+{
+	static public function getSubscribedEvents()
+	{
+		return array(
+			'core.text_formatter_s9e_configure_after' => 'configure_usernamebbcode',
+			'core.text_formatter_s9e_render_after'     => 'prepare_render_usernamebbcode',
+		);
+	}
+	
+	/** @var \phpbb\user_loader */
+	protected $user_loader;
+	
+	/** @var \phpbb\core\template */
+	protected $template;
+	
+	
+	public function __construct(\phpbb\user_loader $user_loader, \phpbb\template\template $template)
+	{
+$user_loader->load_user_by_username('anonymous');
+		$this->user_loader = $user_loader;
+		$this->template = $template;
+	}
+
+	public function configure_usernamebbcode($event)
+	{
+		// Get the BBCode configurator 
+		$configurator = $event['configurator']; 
+
+		// Let's unset any existing BBCode that might already exist 
+		unset($configurator->BBCodes['user']); 
+		unset($configurator->tags['user']); 
+
+		// Let's create the new BBCode 
+		$configurator->BBCodes->addCustom( '[user]{TEXT1}[/user]', '[user]{TEXT1}[/user]');
+		
+	}
+
+	public function prepare_render_usernamebbcode($event)
+	{
+		preg_match_all('#\[user\]\s*([a-zA-Z0-9._\-\/\s]+?)\s*\[\/user\]#is', $event['html'], $tags); 
+		$counter = 1; 
+		for ($i = 0; $i < sizeof($tags[0]); $i++)
+		{
+			$username = $tags[1][$i];
+			$userid = $this->user_loader->load_user_by_username($username);
+			$user = $this->user_loader->get_user($userid,  true);
+			
+			if ( $user == false or $user['username_clean'] == 'anonymous' )
+			{
+				$userbbcode = $username;
+			}
+			else
+			{
+				$userbbcode = $this->user_loader->get_username($userid, 'full', false, false, true);
+			}
+			$event['html'] = str_replace($tags[0][$i], $userbbcode, $event['html']);
+		}
+	}
+
+}
